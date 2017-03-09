4dca08837dd8bdfee99b822247709c18272f0a33
RDP <rdupaw@gmail.com>
2017-03-09 13:03:26 -0600

    Define get_next_target(page) procedure

--- searchEngine.py
+++ searchEngine.py
@@ -6,3 +6,14 @@ end_link = page.find('.com') + 4
 url = page[start_link:end_link]
 
 print url
+
+def get_next_target(page):
+	start_link = page.find('<a href =')
+
+	if start_link < 0:
+		return None, 0
+
+	start_quote = page.find('"', start_link)
+	end_quote = page.find('"', start_quote + 1)
+	url = page[start_quote + 1:end_quote]
+	return url, end_quote
