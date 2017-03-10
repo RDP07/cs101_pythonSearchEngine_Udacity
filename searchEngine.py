5b48a2743b3c9ce9d6c9b0b6e1392c72a3ea5afc
RDP <rdupaw@gmail.com>
2017-03-10 11:58:37 -0600

    Fix spacing on get_next_target

--- searchEngine.py
+++ searchEngine.py
@@ -9,10 +9,8 @@ print url
 
 def get_next_target(page):
 	start_link = page.find('<a href =')
-
 	if start_link < 0:
 		return None, 0
-
 	start_quote = page.find('"', start_link)
 	end_quote = page.find('"', start_quote + 1)
 	url = page[start_quote + 1:end_quote]
