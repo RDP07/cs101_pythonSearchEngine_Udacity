7bdc4784bb4f59e8d51ee3afc48546497d39fe67
RDP <rdupaw@gmail.com>
2017-03-10 12:55:14 -0600

    Define crawl_web procedure

--- searchEngine.py
+++ searchEngine.py
@@ -31,3 +31,13 @@ def get_all_links(page):
 		else:
 			break
 	return links
+
+def crawl_web(seed):
+	tocrawl = [seed]
+	crawled = []
+	while tocrawl: 
+		page = tocrawl.pop()
+		if page not in crawled:
+			union(tocrawl, get_all_links(get_page(page)))
+			crawled.append(page)
+	return crawled
